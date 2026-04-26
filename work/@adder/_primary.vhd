94aff341a6e9ff967c66d3f796177c67d71bad59
yi108 <yi0939674299@gmail.com>
2026-04-27 04:46:51 +0800

    delete

--- work/@adder/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Adder is
-    port(
-        src1            : in     vl_logic_vector(31 downto 0);
-        src2            : in     vl_logic_vector(31 downto 0);
-        sum             : out    vl_logic_vector(31 downto 0)
-    );
-end Adder;
--- work/@j@b_@unit/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity JB_Unit is
-    port(
-        operand1        : in     vl_logic_vector(31 downto 0);
-        operand2        : in     vl_logic_vector(31 downto 0);
-        jb_out          : out    vl_logic_vector(31 downto 0)
-    );
-end JB_Unit;
